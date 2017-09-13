4707ffc85142b197fc6c2dfe3f7abc39101b8f25
Anish Lushte <lushteanish@gmail.com>
2017-09-13 17:54:53 +0530

    Changed as module

--- number-words.js
+++ number-words.js
@@ -1,110 +1,153 @@
-var a = "52122";
-var digitSingle = {
-    '0': null,
-    '1': "One",
-    '2': "Two",
-    '3': "Three",
-    '4': "Four",
-    '5': "Five",
-    '6': "Six",
-    '7': "Seven",
-    '8': "Eight",
-    '9': "Nine"
-};
-
-var digitDouble = {
-    '2': "Twenty",
-    '3': "Thirty",
-    '4': "Forty",
-    '5': "Fifty",
-    '6': "Sixty",
-    '7': "Seventy",
-    '8': "Eighty",
-    '9': "Ninety"
-};
-
-var digitStartWithOne = {
-    '10': 'Ten',
-    '11': 'ELEVEN',
-    '12': 'TWELVE',
-    '13': 'THIRTEEN',
-    '14': 'FOURTEEN',
-    '15': 'FIFTEEN',
-    '16': 'SIXTEEN',
-    '17': 'SEVENTEEN',
-    '18': 'EIGHTEEN',
-    '19': 'NINETEEN'
-};
-
-var input = a.toString().split("");
-var inputLength = input.length;
-
-console.log(input, inputLength);
-
-var hundred = [];
-var thousand = [];
-var lac = [];
-
-if (inputLength < 3) {
-    for (var i = 0; i < 3 - inputLength; i++) {
-        input.unshift("0");
-    }
-} else if (inputLength > 3 && inputLength < 5) {
-    input.unshift("0");
-} else if (inputLength > 5 && inputLength < 7) {
-    input.unshift("0");
-}
-
-if (input.length === 3) {
-    hundred = input.slice(0, 3);
-} else if (input.length === 5) {
-    hundred = input.slice(2, 5);
-    thousand = input.slice(0, 2);
-} else if (input.length === 7) {
-    hundred = input.slice(4, 7);
-    thousand = input.slice(2, 4);
-    lac = input.slice(0, 2);
-}
-
-var hundredString = [];
-var thousandString = [];
-
-
-if (lac.length) {
-
-}
-
-if (thousand.length) {
-    var first = thousand[0];
-    var second = thousand[1];
-    thousandString.unshift('thousand');
-    if (first === '0') {
-        thousandString.unshift(digitSingle[second]);
+(function(root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define([], factory);
+    } else if (typeof exports === 'object') {
+        module.exports = factory();
     } else {
-        if (first === '1') {
-            thousandString.unshift(digitStartWithOne[first + second]);
+        root.anish = factory();
+    }
+})(this, toWordsModule);
+
+function toWordsModule() {
+    var digitSingle = {
+        '0': null,
+        '1': "one",
+        '2': "two",
+        '3': "three",
+        '4': "four",
+        '5': "five",
+        '6': "six",
+        '7': "seven",
+        '8': "eight",
+        '9': "nine"
+    };
+
+    var digitDouble = {
+        '2': "twenty",
+        '3': "thirty",
+        '4': "forty",
+        '5': "fifty",
+        '6': "sixty",
+        '7': "seventy",
+        '8': "eighty",
+        '9': "ninety"
+    };
+
+    var digitStartWithOne = {
+        '10': 'ten',
+        '11': 'eleven',
+        '12': 'twelve',
+        '13': 'thirteen',
+        '14': 'fourteen',
+        '15': 'fifteen',
+        '16': 'sixteen',
+        '17': 'seventeen',
+        '18': 'eighteen',
+        '19': 'nineteen'
+    };
+
+    function setLacAndThousand(first, second, name) {
+        outputA = [];
+        outputA.unshift(name);
+        if (first === '0') {
+            outputA.unshift(digitSingle[second]);
         } else {
-            thousandString.unshift(digitSingle[second]);
-            thousandString.unshift(digitDouble[first]);
+            if (first === '1') {
+                outputA.unshift(digitStartWithOne[first + second]);
+            } else {
+                outputA.unshift(digitSingle[second]);
+                outputA.unshift(digitDouble[first]);
+            }
         }
+
+        return outputA;
     }
+
+    function convertToWords(a) {
+
+        if (isNaN(a)) {
+            throw a + ' this is not valid number.';
+        }
+
+        console.log(a, typeof(a));
+
+        if (!Number.isInteger(a)) {
+            throw 'Please enter only integer value';
+        }
+
+        if (a.toString().length > 7) {
+            throw 'Enter number with 7 or less than 7 digit';
+        }
+
+        var input = a.toString().split("");
+        var inputLength = input.length;
+
+
+        var hundred = [];
+        var thousand = [];
+        var lac = [];
+
+        if (inputLength < 3) {
+            for (var i = 0; i < 3 - inputLength; i++) {
+                input.unshift("0");
+            }
+        } else if (inputLength > 3 && inputLength < 5) {
+            input.unshift("0");
+        } else if (inputLength > 5 && inputLength < 7) {
+            input.unshift("0");
+        }
+
+        if (input.length === 3) {
+            hundred = input.slice(0, 3);
+        } else if (input.length === 5) {
+            hundred = input.slice(2, 5);
+            thousand = input.slice(0, 2);
+        } else if (input.length === 7) {
+            hundred = input.slice(4, 7);
+            thousand = input.slice(2, 4);
+            lac = input.slice(0, 2);
+        }
+
+        var hundredString = [];
+        var thousandString = [];
+        var lacString = [];
+        var first, second, third;
+
+        if (lac.length) {
+            first = lac[0];
+            second = lac[1];
+            lacString = setLacAndThousand(first, second, 'lacs');
+        }
+
+
+        if (thousand.length) {
+            first = thousand[0];
+            second = thousand[1];
+            thousandString = setLacAndThousand(first, second, 'thousand');
+        }
+
+        if (hundred.length) {
+            first = hundred[0];
+            second = hundred[1];
+            third = hundred[2];
+            if (second === '1') {
+                hundredString.unshift(digitStartWithOne[second + third]);
+            } else {
+                hundredString.unshift(digitSingle[third]);
+                hundredString.unshift(digitDouble[second]);
+            }
+            if (first !== '0') {
+                hundredString.unshift('hundred');
+                hundredString.unshift(digitSingle[first]);
+            }
+        }
+
+        return lacString.concat(thousandString).concat(hundredString).join(" ");
+    }
+
+
+    return {
+        ConvertToWords: convertToWords
+    };
+
 }
-
-if (hundred.length) {
-    var first = hundred[0];
-    var second = hundred[1];
-    var third = hundred[2];
-    if (second === '1') {
-        hundredString.unshift(digitStartWithOne[second + third]);
-    } else {
-        hundredString.unshift(digitSingle[third]);
-        hundredString.unshift(digitDouble[second]);
-    }
-    if (first !== '0') {
-        hundredString.unshift('hundred');
-        hundredString.unshift(digitSingle[first]);
-    }
-}
-
-
-console.log(thousandString.concat(hundredString).join(" "));
